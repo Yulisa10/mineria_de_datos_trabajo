8ffa4d21020d7fe54ade112a5b36654a41828bac
Yulisa10 <yortizg@unal.edu.co>
2025-02-10 18:42:54 -0500

    Update trabajo.py

--- trabajo.py
+++ trabajo.py
@@ -37,7 +37,7 @@ def preprocess_data(df):
     scaler = MinMaxScaler()
     X_scaled = scaler.fit_transform(X)
     X_test_scaled = scaler.fit_transform(X)
-    return X_scaled, y, scaler
+    return X_scaled, y, scaler 
 
 X, y, scaler = preprocess_data(df)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
